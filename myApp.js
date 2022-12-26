cc44a35c7225882a8d2925774a4307089181c7f8
Dedios <dediosprogramming@gmail.com>
2022-12-26 18:35:53 -0500

    .env file config

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // trying to get the dotenv import to work 
 let express = require('express');
 let app = express();
 let absolutePath = __dirname + '/views/index.html';
+
 
 
 console.log("Hello World");
@@ -12,7 +14,7 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
-    process.env.MESSAGE_STYLE;
+    process.env.MESSAGE_STYLE; // trying to get the 'dotenv' import to work to use the variable in the .env file
     res.json({
         "message": "HELLO JSON"
     });
