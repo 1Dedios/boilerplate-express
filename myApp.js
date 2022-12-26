949e429b27b7e591adba9fef5485adb4216d84c8
Dedios <dediosprogramming@gmail.com>
2022-12-26 13:17:01 -0500

    express server

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
-// import express from 'express';
-// let app = express();
+let express = require('express');
+let app = express();
+
 
 console.log("Hello World");
+
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+
+module.exports = app;
+
 
