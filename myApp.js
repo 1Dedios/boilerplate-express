ab65fac8b7c6e87252c8287b3b61d0f0d15b0fd5
Dedios <dediosprogramming@gmail.com>
2023-01-23 20:29:03 -0500

    implementing root level middleware

--- myApp.js
+++ myApp.js
@@ -3,15 +3,21 @@ let express = require('express');
 let app = express();
 let absolutePath = __dirname + '/views/index.html';
 
-
-
 console.log("Hello World");
 
 app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 });
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    let method = req.method;
+    let path = req.path;
+    let ip = req.ip;
+    console.log(`${method}` + `${path}` - `${ip}`);
+    next();
+});
+
+// app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
     if (process.env.MESSAGE_STYLE === "uppercase") {
