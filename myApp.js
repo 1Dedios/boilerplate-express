b2a08ec017ca95453052f9eaa745f82502a42e03
Dedios <dediosprogramming@gmail.com>
2023-06-17 00:40:27 -0400

    implementing same middleware

--- myApp.js
+++ myApp.js
@@ -3,14 +3,14 @@ let express = require("express");
 let app = express();
 let absolutePath = __dirname + "/views/index.html";
 
-console.log("Hello World");
-
 app.use((req, res, next) => {
   let string = req.method + " " + req.path + " - " + req.ip;
   console.log(string);
 
   next("route");
 });
+
+console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
