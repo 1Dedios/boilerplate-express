88270c5e4bf2672a3316eaaaaa61a8bbab6e9f13
Dedios <dediosprogramming@gmail.com>
2023-06-16 22:47:29 -0400

    implemented middlware logger for all requests

--- myApp.js
+++ myApp.js
@@ -1,34 +1,32 @@
-require('dotenv').config();
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
-let absolutePath = __dirname + '/views/index.html';
+let absolutePath = __dirname + "/views/index.html";
 
 console.log("Hello World");
 
 app.use((req, res, next) => {
-    let string = req.method + " " + res.path + " - " + req.ip;
-    console.log(string);
-    next('route');
+  let string = req.method + " " + res.path + " - " + req.ip;
+  console.log(string);
+  // next('route');
+  next();
 });
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(absolutePath);
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
 });
 
-
-
-app.get('/json', (req, res) => {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        return res.json({
-            "message": "HELLO JSON"
-        });
-    } return res.json({
-        "message": "Hello json"
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    return res.json({
+      message: "HELLO JSON",
     });
+  }
+  return res.json({
+    message: "Hello json",
+  });
 });
 
 module.exports = app;
-
-
